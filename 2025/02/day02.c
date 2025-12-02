58e1d5fd3afb99464bbf8280d04477885816b641
Sebastian Banert <banert@uni-bremen.de>
2025-12-03 00:29:44 +0100

    Day 2, part 2.

--- 2025/02/day02.c
+++ 2025/02/day02.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Fast integer exponentiation
 long int_pow(long b, long e) {
@@ -30,10 +31,43 @@ long next_invalid(long n) {
   long second_half = n % int_pow(10, d/2);
   if (second_half <= first_half)
     return first_half + first_half *int_pow(10, d / 2);
-  if (first_half + 1 < int_pow(10, d / 2))
-    return first_half + 1 + (first_half + 1) * int_pow(10, d / 2);
-  // unreachable
-  exit(1);
+  return first_half + 1 + (first_half + 1) * int_pow(10, d / 2);
+}
+
+long next_alt_invalid(long n) {
+  long d = num_digits(n);
+  long result = 0;
+
+  // Initialize result with smallest invalid number with one more digit
+  long p = (d+1)/2;
+  while ((d + 1) % p) --p;
+  for (long e = p-1; e < d + 1; e += p) {
+    result += int_pow(10, e);
+  }
+
+  for (p = d/2; p > 0; --p) {
+    if (d % p) continue;
+    long last_chunk = 0;
+    long remainder = n;
+    bool carry_one = false;
+    while (remainder) {
+      long new_chunk = remainder % int_pow(10, p);
+      if (new_chunk > last_chunk) {
+	carry_one = false;
+      } else if (new_chunk < last_chunk) {
+        carry_one = true;
+      }
+      last_chunk = new_chunk;
+      remainder /= int_pow(10, p);
+    }
+    if (carry_one) ++last_chunk;
+    long attempt = 0;
+    for (long e = 0; e < d; e += p) {
+      attempt += last_chunk * int_pow(10, e);
+    }
+    if (attempt < result) result = attempt;
+  }
+  return result;
 }
 
 int main(void) {
@@ -48,19 +82,22 @@ int main(void) {
   char *next_char = line;
   long lower, upper;
   long sum_invalid = 0;
+  long sum_alt_invalid = 0;
   do {
     lower = strtol(next_char, &next_char, 10);
     upper = strtol(next_char + 1, &next_char, 10);
     while (lower <= upper) {
-      long n = next_invalid(lower);
-      if (n <= upper)
-        sum_invalid += n;
+      long n = next_alt_invalid(lower);
+      if (n <= upper) {
+        sum_alt_invalid += n;
+	if (next_invalid(n) == n) sum_invalid += n;
+      }
       lower = n + 1;
     }
     next_char++;
   } while (*next_char);
 
-  printf("%ld\n", sum_invalid);
+  printf("%ld\n%ld\n", sum_invalid, sum_alt_invalid);
 
   free(line);
 }
