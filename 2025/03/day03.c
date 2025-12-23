bdd11b4a42446db46da5d6a373d07d32bd450552
Sebastian Banert <banert@uni-bremen.de>
2025-12-23 22:39:12 +0100

    Day 3, part 2.

--- 2025/03/day03.c
+++ 2025/03/day03.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-long get_joltage(char *l) {
+// Specialised function for part 1
+long get_joltage2(char *l) {
   char first = *l;
   char second = *(l + 1);
   for (++l; '0' <= *l && *l <= '9'; ++l) {
@@ -15,19 +17,51 @@ long get_joltage(char *l) {
   return 10 * (first - '0') + (second - '0');
 }
 
+long get_joltage(size_t b, const char *l) {
+  // Containing the positions of the result digits in the string
+  // Initialize with 0, ..., b-1
+  size_t digits[b];
+  for (size_t i = 0; i < b; ++i)
+    digits[i] = i;
+  // Effective length of the digit sequence;
+  // results from getline contain newline.
+  // Might not be portable to Windows (where long is 4 bytes anyway).
+  size_t len = strlen(l) - 1;
+
+  for (size_t i = 1; i < len; ++i) {
+    for (size_t j = 0; j < b; ++j) {
+      if (i > digits[j] && l[i] > l[digits[j]] && b + i <= len + j) {
+	for (size_t k = 0; j + k < b; ++k) {
+	  digits[j + k] = i + k;
+	}
+	break;
+      }
+    }
+  }
+
+  long result = 0;
+  for (size_t i = 0; i < b; ++i) {
+    result *= 10;
+    result += l[digits[i]] - '0';
+  }
+  return result;
+}
+
 int main(void) {
   char *line = NULL;
   size_t linecapp = 0;
   ssize_t nchars;
 
   long sum_joltages = 0;
+  long sum_big_joltages = 0;
   while(1) {
     nchars = getline(&line, &linecapp, stdin);
     if (nchars < 2) break;
-    sum_joltages += get_joltage(line);
+    sum_joltages += get_joltage(2, line);
+    sum_big_joltages += get_joltage(12, line);
   }
 
-  printf("%ld\n", sum_joltages);
+  printf("%ld\n%ld\n", sum_joltages, sum_big_joltages);
 
   free(line);
 }
